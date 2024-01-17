f9a27a55f3771ab6e5020466015cea43e21576f9
Axxxll <alex.plam.atan@gmail.com>
2024-01-17 15:31:57 +0200

    Creating Data Context

---
+++ backend/UserProjects/Data/DataContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TimeLog> TimeLogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+        .HasMany(u => u.TimeLogs)
+        .WithOne(t => t.User)
+        .HasForeignKey(t => t.UserId);
+
+        modelBuilder.Entity<Project>()
+            .HasMany(p => p.TimeLogs)
+            .WithOne(t => t.Project)
+            .HasForeignKey(t => t.ProjectId);
+    }
+}
