82dc4b705ebf143d82f66a60f15c9347c240eeb7
Axxxll <alex.plam.atan@gmail.com>
2024-01-17 15:04:49 +0200

    Adding Models

---
+++ backend/UserProjects/Models/TimeLog.cs
@@ -0,0 +1,10 @@
+public class TimeLog 
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public int ProjectId { get; set; }
+    public Project Project { get; set; }
+    public DateTime Date { get; set; }
+    public float HoursWorked { get; set; }
+}
